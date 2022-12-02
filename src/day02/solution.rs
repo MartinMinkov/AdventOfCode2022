469bd714a48b118c12587ac98068df05dca80d60
Martin Minkov <minkovlmartin@gmail.com>
2022-12-02 14:47:23 -0800

    day02

--- src/day02/solution.rs
+++ src/day02/solution.rs
@@ -1,35 +1,79 @@
-use crate::helpers::read_input;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-
-type ElfCalories = u32;
+use std::collections::HashMap;
+use std::fs::read_to_string;
 
 pub fn solve() {
-    let path = "./src/day01/input.txt";
-    let input = read_input(path).unwrap();
-    let mut elf_calories = get_elf_calories(input);
-    elf_calories.sort();
+    let path = "./src/day02/input.txt";
+    let input = read_to_string(path).expect("Should have been able to read the file");
 
-    let max = elf_calories.iter().max().unwrap();
-    println!("Max calories of top 1: {}", max);
+    let total_points1 = calculate_points(&input, init_hashmap1());
+    let total_points2 = calculate_points(&input, init_hashmap2());
 
-    let sum = elf_calories.iter().rev().take(3).sum::<ElfCalories>();
-    println!("Calories of top 3: {}", sum);
+    println!("Total points: {}", total_points1);
+    println!("Total points: {}", total_points2);
 }
 
-fn get_elf_calories(input: BufReader<File>) -> Vec<ElfCalories> {
-    let mut elf_calories = Vec::new();
-    let mut current_calories = 0;
+fn calculate_points(input: &str, hashmap_outcomes: HashMap<&str, i32>) -> i32 {
+    let mut total_points = 0;
 
-    for line in input.lines() {
-        let line = line.unwrap();
-        if line == "" {
-            elf_calories.push(current_calories);
-            current_calories = 0;
-        } else {
-            let calories = line.parse::<ElfCalories>().unwrap();
-            current_calories += calories;
-        }
+    for line in input.split("\n") {
+        let outcome = hashmap_outcomes
+            .get(line)
+            .expect("Could not find outcome in hashmap");
+        total_points += outcome
     }
-    elf_calories
+    total_points
+}
+
+pub fn init_hashmap1<'a>() -> HashMap<&'a str, i32> {
+    let rock_points = 1;
+    let paper_points = 2;
+    let scissors_points = 3;
+
+    let win_points = 6;
+    let draw_points = 3;
+    let lose_points = 0;
+
+    let mut hashmap_outcomes = HashMap::new();
+    // A = Rock, B = Paper, C = Scissors
+    // X = Rock, Y = Paper, Z = Scissors
+    hashmap_outcomes.insert("A X", draw_points + rock_points);
+    hashmap_outcomes.insert("A Y", win_points + paper_points);
+    hashmap_outcomes.insert("A Z", lose_points + scissors_points);
+
+    hashmap_outcomes.insert("B X", lose_points + rock_points);
+    hashmap_outcomes.insert("B Y", draw_points + paper_points);
+    hashmap_outcomes.insert("B Z", win_points + scissors_points);
+
+    hashmap_outcomes.insert("C X", win_points + rock_points);
+    hashmap_outcomes.insert("C Y", lose_points + paper_points);
+    hashmap_outcomes.insert("C Z", draw_points + scissors_points);
+
+    hashmap_outcomes
+}
+
+pub fn init_hashmap2<'a>() -> HashMap<&'a str, i32> {
+    let rock_points = 1;
+    let paper_points = 2;
+    let scissors_points = 3;
+
+    let win_points = 6;
+    let draw_points = 3;
+    let lose_points = 0;
+
+    let mut hashmap_outcomes = HashMap::new();
+    // A = Rock, B = Paper, C = Scissors
+    // X = Lose, Y = Draw, Z = Win
+    hashmap_outcomes.insert("A X", lose_points + scissors_points);
+    hashmap_outcomes.insert("A Y", draw_points + rock_points);
+    hashmap_outcomes.insert("A Z", win_points + paper_points);
+
+    hashmap_outcomes.insert("B X", lose_points + rock_points);
+    hashmap_outcomes.insert("B Y", draw_points + paper_points);
+    hashmap_outcomes.insert("B Z", win_points + scissors_points);
+
+    hashmap_outcomes.insert("C X", lose_points + paper_points);
+    hashmap_outcomes.insert("C Y", draw_points + scissors_points);
+    hashmap_outcomes.insert("C Z", win_points + rock_points);
+
+    hashmap_outcomes
 }
